749c225d79fe6bd228c30bd589df78172a35b34a
632366134 <632366134@qq.com>
2023-08-25 17:35:04 +0800

    相机元数据

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -6,59 +6,53 @@ Page({
         renderWidth: 300,
         renderHeight: 300,
         texts: [],
+        xrFlag: false,
         obsListRaw: [{
-                id: 1,
-                projectCode: 1,
-                url: "https://arp3.arsnowslide.com/undefined/319292761266671616/undefined/1674012576482.png"
-            },
-            {
-                id: 4,
-                projectCode: 2,
-                url: "https://arp3.arsnowslide.com/undefined/319293236959465472/undefined/1671501529227.png"
-            }
-        ],
-        videoListRaw: [{
-                id: 2,
-                projectCode: 1,
-                url: "https://arp3.arsnowslide.com/undefined/319292761266671616/undefined/app~1.mp4",
-                scale: [1, 1, 1],
-                position: [0, -2, 0],
-                rotation: [0, 0, 0],
-                opacity: false
-            },
-            {
-                id: 5,
-                projectCode: 2,
-                url: "https://arp3.arsnowslide.com/undefined/319292761266671616/undefined/app~1.mp4",
-                scale: [1, 1, 1],
-                position: [0, 0, 0],
-                rotation: [0, 0, 0],
-                opacity: true
-            }
+            id: 1,
+            projectCode: 1,
+            url: "https://arp3.arsnowslide.com/undefined/319292761266671616/undefined/1674012576482.png"
+        }],
+        videoListRaw: [
+
+            // {
+            //     id: 2,
+            //     projectCode: 1,
+            //     url: "https://arp3.arsnowslide.com/undefined/319292761266671616/undefined/app~1.mp4",
+            //     scale: [1, 1, 1],
+            //     position: [0, -2, 0],
+            //     rotation: [0, 0, 0],
+            //     opacity: false
+            // },
+            // {
+            //     id: 5,
+            //     projectCode: 2,
+            //     url: "https://arp3.arsnowslide.com/undefined/319292761266671616/undefined/app~1.mp4",
+            //     scale: [1, 1, 1],
+            //     position: [0, 0, 0],
+            //     rotation: [0, 0, 0],
+            //     opacity: true
+            // }
         ],
         gltfListRaw: [{
             id: 6,
-            scale: [0.01, 0.01, 0.01],
-            position: [0, 0, 1],
-            rotation: [0, 0, 0],
+            scale: [0.1, 0.1, 0.1],
+            position: [0, 0, 0.6],
+            rotation: [-90, 0, 0],
             projectCode: 1,
-            url: "https://arp3.arsnowslide.com/undefined/319565566776397824/undefined/index.glb"
-        }, {
-            id: 3,
-            scale: [0.01, 0.01, 0.01],
-            position: [0, 0, 1],
-            rotation: [0, 0, 0],
-            projectCode: 2,
-            url: "https://arp3.arsnowslide.com/undefined/319565566776397824/undefined/index.glb",
+            // url: "https://arp3.arsnowslide.com/undefined/396100883129520128/undefined/huipaijianzhu.glb",
+            url: "/assets/model3.glb",
+
             name: 'parent'
         }],
         keyframeListRaw: [{
+            parentId: 6,
             id: 3,
             keyframe: '/assets/animation/miku.json',
             name: 'parent'
         }]
     },
     onLoad() {
+
         const info = wx.getSystemInfoSync();
         const width = info.windowWidth;
         const height = info.windowHeight;
@@ -67,7 +61,89 @@ Page({
             width,
             height,
             renderWidth: width * dpi,
-            renderHeight: height * dpi
+            renderHeight: height * dpi,
+            xrFlag: true
         });
+
+
+    },
+    onUnload() {
+        this.setData({
+            xrFlag: false
+        })
+
+    },
+    arr({
+        detail
+    }) {
+
+        console.log({
+            detail
+        })
+        const dataURL = this.convertYUV420ToDataURL(detail.uvBuffer, detail.width, detail.height)
+console.log(dataURL); // 输出生成的数据 URL
+    },
+    convertYUV420ToDataURL(yuv420ArrayBuffer, width, height) {
+        const query = wx.createSelectorQuery()
+        query.select('#myCanvas')
+            .fields({
+                node: true,
+                size: true
+            })
+            .exec((res) => {
+                const canvas = res[0].node
+                const uint8Array = new Uint8Array(yuv420ArrayBuffer);
+                const context = canvas.getContext('2d')
+
+
+                function yuv420ToRgb(y, u, v) {
+                    const r = y + 1.13983 * v;
+                    const g = y - 0.39465 * u - 0.58060 * v;
+                    const b = y + 2.03211 * u;
+                    return [r, g, b];
+                }
+
+                const imageData = context.createImageData(width, height);
+                let dataIndex = 0;
+                for (let i = 0; i < uint8Array.length; i += 1.5) {
+                    const y = uint8Array[i];
+                    const u = uint8Array[Math.floor(i / 4) + width * height];
+                    const v = uint8Array[Math.floor(i / 4) + width * height * 1.25];
+                    const [r, g, b] = yuv420ToRgb(y, u, v);
+
+                    imageData.data[dataIndex++] = r;
+                    imageData.data[dataIndex++] = g;
+                    imageData.data[dataIndex++] = b;
+                    imageData.data[dataIndex++] = 255;
+
+                    if (i % (width * 1.5) === 0 && i > 0) {
+                        dataIndex += (width - 1) * 4;
+                    }
+                }
+
+                context.putImageData(imageData, 0, 0);
+
+                return canvas.toDataURL('image/jpeg'); // 根据需要选择图片格式
+
+            })
     }
+    // tapFuction({
+    //     detail
+    // }) {
+    //     console.log(detail.data)
+    //     wx.showModal({
+    //         title: 'tap',
+    //         content: detail.data.url,
+    //         complete: (res) => {
+    //             if (res.cancel) {
+
+    //             }
+
+    //             if (res.confirm) {
+
+    //             }
+    //         }
+    //     })
+    // }
+
 })
--- miniprogram/src/xr-canvas.js
+++ miniprogram/src/xr-canvas.js
@@ -3,6 +3,8 @@ const STATE = {
     MOVE: 0,
     ZOOM_OR_PAN: 1
 }
+let prevValue;
+
 import './remove-black';
 Component({
     properties: {
@@ -18,9 +20,9 @@ Component({
             type: Array,
             default: []
         },
-        keyframeListRaw:{
-            type:Array,
-            default:[]
+        keyframeListRaw: {
+            type: Array,
+            default: []
         }
     },
     observers: {
@@ -39,22 +41,107 @@ Component({
                 obsList: newVal,
             })
         },
-        keyframeListRaw(newVal){
+        keyframeListRaw(newVal) {
             this.setData({
                 keyframeList: newVal,
             })
         }
     },
+
     data: {
         loaded: false,
         arReady: false,
+        gltfFlag: false
     },
     lifetimes: {
         attached() {
             console.log('data', this.data)
-        }
+        },
+        detached() {
+            if (this.data.videoList && this.data.videoList.length > 0) {
+                const scene = this.scene
+                this.data.videoList.map((i) => {
+                    // 释放加载过的资源
+                    scene.assets.releaseAsset('video-texture', `${i.projectCode}`);
+                    scene.assets.releaseAsset('material', `${i.id}-video-mat`);
+                })
+            }
+            if (this.data.gltfList && this.data.gltfList.length > 0) {
+                const scene = this.scene
+
+
+                this.data.gltfList.map((v) => {
+                    // 释放加载过的资源
+                    scene.assets.releaseAsset('gltf', `gltf-${v.id}`);
+                })
+            }
+            if (this.scene) {
+                this.scene = null
+            }
+            if (this.anchor) {
+                this.anchor = null
+            }
+            if (this.trs) {
+                this.trs = null
+            }
+            if (this.GLTF) {
+                this.GLTF = null
+            }
+            if (this.xrgltf) {
+                this.xrgltf = null
+            }
+            if (this.tmpV3) {
+                this.tmpV3 = null
+            }
+            if (this.gltfModel) {
+                this.gltfModel = null
+            }
+            if (this.gltfModel) {
+                this.gltfModel = null
+            }
+            if (this.gltfModel) {
+                this.gltfModel = null
+            }
+            if (this.gltfItemSubTRS) {
+                this.gltfItemSubTRS = null
+            }
+            if (this.videoTRS) {
+                this.videoTRS = null
+            }
+            if (this.gltfItemTRS) {
+                this.gltfItemTRS = null
+            }
+        },
     },
     methods: {
+        createComparator(value) {
+            const mesh = this.scene.getElementById(value)
+
+            if (prevValue === undefined) {
+                prevValue = value;
+                const shape = mesh.addComponent(this.xrFrameSystem.CubeShape, {
+                    autoFit: true
+                })
+            }
+
+            if (prevValue === value) {
+                return
+            } else {
+                const shape = mesh.addComponent(this.xrFrameSystem.CubeShape, {
+                    autoFit: true
+                })
+                const Oldmesh = this.scene.getElementById(prevValue)
+                Oldmesh.removeComponent(this.xrFrameSystem.CubeShape)
+                prevValue = value;
+            }
+        },
+        goPriview({
+            currentTarget
+        }) {
+            // this.triggerEvent("modalFlagTap", {
+            //     data: currentTarget.dataset.item
+            // })
+        },
         handleTrackerSwitch({
             detail
         }) {
@@ -68,39 +155,92 @@ Component({
                 const markerTracker = this.scene.getElementById(`marker-${markerInfo.projectCode}`)
 
                 if (element === markerTracker) {
+
                     if (active) {
-                        const video = this.scene.assets.getAsset('video-texture', markerInfo.projectCode);
+                        const gltfId = this.data.gltfList.filter(v => {
+                            return v.projectCode === markerInfo.projectCode
+                        })
+                        console.log(gltfId)
+                        this.createComparator(markerInfo.projectCode)
+                        const video = this.video = this.scene.assets.getAsset('video-texture', markerInfo.projectCode);
                         this.videoTRS = this.scene.getElementById(`videoNode-${markerInfo.projectCode}`)?.getComponent(this.xrFrameSystem.Transform)
                         console.log(markerInfo.projectCode, this.videoTRS, video, '123')
-
                         if (this.videoTRS && video) {
                             console.log('havevideo')
                             const d = video.width / video.height
                             this.videoTRS.scale.x = 1 * d
 
-                            video.play()
+                            video && video.play()
                         }
                         this.gltfItemTRS = this.scene.getElementById(`gltfNode-${markerInfo.projectCode}`)?.getComponent(this.xrFrameSystem.Transform)
                         this.gltfItemSubTRS = this.scene.getElementById(markerInfo.projectCode)?.getComponent(this.xrFrameSystem.Transform)
+                        this.gltfAnim = this.scene.getElementById(markerInfo.projectCode)?.getComponent(this.xrFrameSystem.Animator)
+
                         // 开启旋转缩放逻辑
                         if (this.gltfItemTRS) {
                             this.scene.event.addOnce('touchstart', this.handleTouchStart)
 
                         }
+                        if (this.data.keyframeList.length > 0) {
+                            this.data.keyframeList.forEach(async (obj, index) => {
+                                if (obj.parentId === gltfId[0].id) {
+                                    this.AnimatorKeyId = this.scene.getElementById(`keyframe-${gltfId[0].id}`)
+                                    this.AnimatorKey = this.AnimatorKeyId.getComponent(this.xrFrameSystem.Animator)
+                                    await this.AnimatorKey.play(obj.name)
+                                    await this.addGltfAnim()
 
+
+                                }
+                            });
+                        } else {
+                            await addGltfAnim()
+                        }
+                        // this.detached2()
                         // console.log(this.xrFrameSystem)
-                        // const ARSystem = this.scene.getElementById(`xr-scene`).getComponent(this.xrFrameSystem.ARSystem)
-                        // const arr = ARSystem.getARRawData()
-                        // console.log(arr)
+                        const ARSystem = this.scene.getElementById(`xr-scene`).getComponent(this.xrFrameSystem.ARSystem)
+                        const arr = ARSystem.getARRawData()
+                        console.log(arr)
                         // let encodeStr =encode(arr.uvBuffer); //加密
                         // console.log(encodeStr);
+                        this.triggerEvent('arr',arr)
 
+                    } else {
+                        this.video && this.video.pause()
+                        this.setData({
+                            gltfFlag: false
+                        })
+                        if (this.gltfAnim?.stop !== undefined) {
+                            this.gltfAnim.stop()
+
+                        }
                     }
 
                 }
 
             })
 
+        },
+        async addGltfAnim() {
+            if (this.gltfAnim?._clips !== undefined) {
+                const clips = this.gltfAnim._clips;
+                this.clipName = []
+
+                clips.forEach((v, key) => {
+                    this.clipName.push(key)
+                })
+                if (this.clipName.length != 0) {
+                    this.clipName.forEach(i => {
+                        this.gltfAnim.play(i, {
+                            loop: -1,
+                        });
+                    })
+                }
+
+            }
+
+            await this.setData({
+                gltfFlag: true
+            })
         },
         handleReady({
             detail
@@ -198,7 +338,7 @@ Component({
                     return
                 }
                 this.gltfItemTRS.rotation.x -= phi
-                this.gltfItemSubTRS.rotation.y -= theta
+                this.gltfItemSubTRS.rotation.z += theta
                 this.mouseInfo.startX = x
                 this.mouseInfo.startY = y
             }
@@ -250,6 +390,53 @@ Component({
             // this.setData({
             //     arReady: true
             // })
+        },
+        convertYUV420ToDataURL(yuv420ArrayBuffer, width, height) {
+            const uint8Array = new Uint8Array(yuv420ArrayBuffer);
+
+            const offscreenCanvas = wx.createOffscreenCanvas(width, height);
+            const canvasContext = offscreenCanvas.getContext('webgl');
+
+            function yuv420ToRgb(y, u, v) {
+                const r = y + 1.13983 * v;
+                const g = y - 0.39465 * u - 0.58060 * v;
+                const b = y + 2.03211 * u;
+                return [r, g, b];
+            }
+
+            const imageData = offscreenCanvas.createImageData(width, height);
+            let dataIndex = 0;
+            for (let i = 0; i < uint8Array.length; i += 1.5) {
+                const y = uint8Array[i];
+                const u = uint8Array[Math.floor(i / 4) + width * height];
+                const v = uint8Array[Math.floor(i / 4) + width * height * 1.25];
+                const [r, g, b] = yuv420ToRgb(y, u, v);
+
+                imageData.data[dataIndex++] = r;
+                imageData.data[dataIndex++] = g;
+                imageData.data[dataIndex++] = b;
+                imageData.data[dataIndex++] = 255;
+
+                if (i % (width * 1.5) === 0 && i > 0) {
+                    dataIndex += (width - 1) * 4;
+                }
+            }
+
+            canvasContext.putImageData(imageData, 0, 0);
+
+            return new Promise((resolve, reject) => {
+                offscreenCanvas.toTempFilePath({
+                    fileType: 'jpg', // 根据需要选择图片格式
+                    success: (res) => {
+                        resolve(res.tempFilePath);
+                    },
+                    fail: (error) => {
+                        reject(error);
+                    },
+                });
+            });
         }
+
+
     }
 })
